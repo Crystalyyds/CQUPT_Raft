4e66f83cc2d341d2cd5c122bc799227e8e3e7965
Yang JiLei <3629152238@qq.com>
2026-03-04 13:49:32 +0800

    init

---
+++ build/CMakeFiles/CQUPT_Raft.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CQUPT_Raft.
+# This may be replaced when dependencies are built.
---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main(int, char**){
+    std::cout << "Hello, from CQUPT_Raft!\n";
+}
